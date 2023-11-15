37154c43f64ada91a2355ab0c4f4302f74a9cbed
Toy Rik <toyrik@gmail.com>
2023-11-15 06:58:26 +0300

    Fix Https для прода

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if (env('APP_FORCE_HTTPS', false)) {
-            URL::forceSceme('https');
+            URL::forceScheme('https');
         }
     }
 }
